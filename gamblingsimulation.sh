d168c46ca671fb31eb8f287ff1d26aef1080b364
priyalisakhare31@gmail.com <priyalisakhare31@gmail.com>
2020-06-09 20:29:59 +0530

    Added gambler bet function won or loss

--- gamblingsimulation.sh
+++ gamblingsimulation.sh
@@ -1,7 +1,24 @@
 #!/bin/bash -x
-echo "Gambling Simulator"
-
 INITIAL_STAKE=100
 BET=1
+DAY=1
+WIN=1
+LOSS=0
 
-echo "Initial Stake $INITIAL_STAKE"
+echo "gambling simulator"
+
+function result() 
+{
+        rem=$(( RANDOM%2 ))
+
+        if [ $rem -eq 1 ]
+        then
+                INITIAL_STAKE=$(( INITIAL_STAKE+1 ))
+        else
+                INITIAL_STAKE=$(( INITIAL_STAKE-1 ))
+        fi
+}
+
+result
+
+echo $INITIAL_STAKE
